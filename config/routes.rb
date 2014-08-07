a894a6e17b4a8ace0b4d47ca2f453cde92f35a92
Amardeep Jadhav <amardeep.jadhav@axenic.co.in>
2014-08-07 16:32:43 +0530

    complete CRUD operation on Grading module and done validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
   
+  
+
+  
+
+  
+
+  resources :gradinglevles
+  
+  root 'gradinglevles#index'
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
