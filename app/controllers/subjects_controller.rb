28d491b37dfc939847778ef6fd74c6924d5e67e1
sud <sud@sudo.(none)>
2014-08-21 09:54:19 +0530

    Edu

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,8 @@
 class SubjectsController < ApplicationController
 
   before_action :set_subject, only:[:edit,:update,:destroy]
-  
+  respond_to :html, :js
+
   	def index
         @batches=Batch.all
     end
@@ -16,20 +17,19 @@ class SubjectsController < ApplicationController
 
       @elective_group=ElectiveGroup.find(params[:elective_group_id]) if params[:elective_group_id]
       @subject=@elective_group.subjects.build if params[:elective_group_id]
- 
+
   	end
   	
   	def create
   		  @batch=Batch.find(params[:batch_id])
+        @subjects=@batch.subjects.all
+          @elective_groups=@batch.elective_groups.all
         @subject=@batch.subjects.new(subject_params)
 
         @elective_group=ElectiveGroup.find params[:elective_group_id] if params[:elective_group_id]
         @subject=@elective_group.subjects.new(subject_params) if params[:elective_group_id]
-        if @subject.save
-         redirect_to batch_subjects_path, notice: "Subject was successfully created"
-        else
-         render 'new'
-        end
+        
+        @subject.save
   	end
 
     def show
@@ -42,25 +42,22 @@ class SubjectsController < ApplicationController
     end
 
     def update
-      if @subject.update(subject_params)
-         redirect_to batch_subjects_path, notice: "Subject was successfully updated"
-      else
-        render 'new'
-      end
+        @subjects=@batch.subjects.all
+          @elective_groups=@batch.elective_groups.all
+      @subject.update(subject_params)
     end
 
     def destroy
-     if @subject.destroy
-        redirect_to batch_subjects_path, notice: "Subject was successfully deleted"
-      else
-        redirect_to batch_subjects_path, notice: "Subject was uanable to delete"
-      end
+       @subjects=@batch.subjects.all
+          @elective_groups=@batch.elective_groups.all
+      @subject.destroy
     end
 
   	private
     def set_subject
       @batch=Batch.find(params[:batch_id])
       @subject=@batch.subjects.find(params[:id])
+      @subject=@elective_group.subjects.find(params[:elective_group_id]) if params[:elective_group_id]
     end
 
 	  def subject_params
--- config/routes.rb
+++ config/routes.rb
@@ -23,13 +23,16 @@ end
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :subjects
+  resources :subjects 
    resources :batches do
     resources :subjects
-    resources :elective_groups do
+    resources :elective_groups  do
      resources :subjects 
-    end
-  end  
+    end 
+  end 
+  resources :elective_groups do
+     resources :subjects 
+    end 
   get "subjects/select"
   # Example resource route with options:
   #   resources :products do
