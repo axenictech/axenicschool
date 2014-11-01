6933d7faa2ec87c9bdfa5b3489ef166094baec61
Amardeep Jadhav <amardeep.jadhav@axenic.co.in>
2014-11-01 19:14:27 +0530

    1 nov 2014 updated backlog and paysleep

---
+++ db/migrate/20141101072311_create_employee_salery_structures.rb
@@ -0,0 +1,10 @@
+class CreateEmployeeSaleryStructures < ActiveRecord::Migration
+  def change
+    create_table :employee_salery_structures do |t|
+      t.references :employee,index:true
+      t.references :payroll_category,index:true
+      t.string     :amount
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/employee_salery_structure_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EmployeeSaleryStructureTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
