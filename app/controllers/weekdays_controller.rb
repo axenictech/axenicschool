f92e811bdb96d9fa7ff492b88c37b6de1c723abd
Anuja <anujaraut24@gmail.com>
2014-12-29 15:12:32 +0530

    TimeTableOptim

--- app/controllers/weekdays_controller.rb
+++ app/controllers/weekdays_controller.rb
@@ -11,23 +11,13 @@ class WeekdaysController < ApplicationController
   def create
     @day = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
     @days = %w(0 1 2 3 4 5 6)
-
     @batch = params[:weekday][:batch_id]
-
-    @batch = @batch = Batch.find(params[:weekday][:batch_id])
-    week = []
+    @batch = Batch.find(params[:weekday][:batch_id])
     week = params[:weekdays]
-
-    @weekdays = Weekday.where(batch_id: @batch.id)
-    unless @weekdays.nil?
-      @weekdays.destroy_all
-    end
-    week.each  do |w|
-      divas = @day[w.to_i]
-      weekday = Weekday.new(batch_id: @batch.id, weekday: divas)
-      weekday.save
-    end
-    flash[:notice] = 'Weekdays created successfully'
+    @weekdays = Weekday.batchid(@batch)
+    @week = Weekday.deleteweek(@weekdays)
+    @weekday = Weekday.saveweek(week, @day, @batch)
+    flash[:notice] = 'Weekday created successfully......'
   end
 
   def select
--- app/models/weekday.rb
+++ app/models/weekday.rb
@@ -1,4 +1,19 @@
 class Weekday < ActiveRecord::Base
   belongs_to :batch
   scope :shod, ->(id) { where(id: id).take }
+   scope :batchid, ->(b) { where(batch_id: b.id) }
+  def self.deleteweek(w)
+  	 unless w.nil?
+        w.destroy_all
+      end
+    w
+  end
+
+  def self.saveweek(w,d,b)
+  	 w.each  do |wa|
+      divas = d[wa.to_i]
+     weekday = Weekday.new(batch_id: b.id, weekday: divas)
+       weekday.save
+     end
+  end
 end
