48a5a3c721128ca663ddfb91d89eb9a1559918b3
ravikiran kadam <ravikirankadam.1991@gmail.com>
2015-01-05 17:35:45 +0530

    code optimization of exams and exam setting

--- app/models/course.rb
+++ app/models/course.rb
@@ -13,4 +13,42 @@ class Course < ActiveRecord::Base
   has_many :class_designations
   has_many :ranking_levels
   scope :shod, ->(id) { where(id: id).take }
+
+  def max(max_rank, rank_lev1)
+    max_rank = 0.to_i if max_rank.nil?
+    rank_lev1.prioriy = max_rank + 1.to_i
+    rank_lev1
+  end
+
+  def increase_logic(rank_levels, selected)
+    previous = 0.to_i
+    temp = nil
+    rank_levels.each do |p|
+      temp = p if (selected == previous)
+      if (selected + 1.to_i == previous)
+        current = p.prioriy
+        previous_pri = temp.prioriy
+        p.update(prioriy: nil)
+        temp.update(prioriy: current)
+        p.update(prioriy: previous_pri)
+      end
+      previous += 1.to_i
+    end
+  end
+
+  def decrease_logic(rank_levels, selected)
+    next_pri = 0.to_i
+    temp = nil
+    rank_levels.each do |p|
+      temp = p if (selected - 1.to_i == next_pri)
+      if (selected == next_pri)
+        current = p.prioriy
+        next_priority = temp.prioriy
+        p.update(prioriy: nil)
+        temp.update(prioriy: current)
+        p.update(prioriy: next_priority)
+      end
+      next_pri += 1.to_i
+    end
+  end
 end
