c927ccb3e964096ef717cc7d904eccd982c867f2
ravikiran kadam <ravikirankadam.1991@gmail.com>
2015-01-06 10:14:09 +0530

    learn helper from anuja

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,12 @@
 module StudentsHelper
+	def admission1_error!
+    return if @student.errors.empty?
+    messages = @student.errors.full_messages.map { |m| content_tag(:li, m) }.join
+    html = <<-HTML
+     <div class="alert alert-danger alert-dismissable">
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+    html.html_safe
+  end
 end
