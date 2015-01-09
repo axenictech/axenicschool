d4027aece483d8b9af0fff38787b5a00ac1a03b5
sachin <you@example.com>
2015-01-09 17:19:41 +0530

    ERP

--- app/helpers/finance_helper.rb
+++ app/helpers/finance_helper.rb
@@ -1,2 +1,12 @@
 module FinanceHelper
+	def fee_errors!
+    return if @fee.errors.empty?
+    messages = @fee.errors.full_messages.map { |m| content_tag(:li, m) }.join
+    html = <<-HTML
+     <div class="alert alert-danger alert-dismissable">
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+    html.html_safe
+  end
 end
