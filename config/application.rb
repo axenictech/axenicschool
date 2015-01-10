bb737d6c7f457654298d6185ffc87b3062df60f6
Sudarshan Dhokale <sudarshandhokale09@gmail.com>
2015-01-10 15:00:59 +0530

    assest precompile for images and vendor.js and vendor.css file

--- config/application.rb
+++ config/application.rb
@@ -8,23 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Educulture
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-    # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = 'utf-8'
     config.active_record.whitelist_attributes :false
-    # Configure sensitive parameters which will be filtered from the log file.
     config.middleware.use 'PDFKit::Middleware', print_media_type: true
     config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
     config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+    config.assets.precompile += %w(vendor.css vendor.js)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
