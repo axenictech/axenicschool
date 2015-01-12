33682363f4a712bbcffd080624d2d06f603f3895
Sudarshan Dhokale <sudarshandhokale09@gmail.com>
2015-01-12 10:59:25 +0530

    turbolinks gem added to this for fast development

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
  Rails.application.config.assets.version = '1.0'
  Rails.application.config.assets.precompile += %w( vendor.css vendor.js )
  Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+ Rails.application.config.assets.precompile += %w( glyphicons-halflings-regular.eot glyphicons-halflings-regular.svg glyphicons-halflings-regular.ttf glyphicons-halflings-regular.woff)
