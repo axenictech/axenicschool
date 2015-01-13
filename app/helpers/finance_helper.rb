d6fa47f252bc1f61727764c7db13f8e8a5aa3619
ravikiran kadam <ravikirankadam.1991@gmail.com>
2015-01-13 10:18:14 +0530

    push for removing merging of finance

--- app/helpers/finance_helper.rb
+++ app/helpers/finance_helper.rb
@@ -1,10 +1,6 @@
 # finance helper
 module FinanceHelper
-<<<<<<< HEAD
   def particular_error!
-=======
-  def fee_errors!
->>>>>>> b96094dbbc906571a04744ef07e3a0a833e26c5e
     return if @fee.errors.empty?
     messages = @fee.errors.full_messages.map { |m| content_tag(:li, m) }.join
     html = <<-HTML
@@ -15,7 +11,6 @@ module FinanceHelper
     html.html_safe
   end
 
-<<<<<<< HEAD
   def collection_error!
     return if @collection.errors.empty?
     messages = @collection.errors.full_messages.map { |m| content_tag(:li, m) }.join
@@ -25,10 +20,6 @@ module FinanceHelper
     </div>
     HTML
     html.html_safe
-=======
-  def collect_category
-    @categories.collect { |c| [c.name, c.id] }
->>>>>>> b96094dbbc906571a04744ef07e3a0a833e26c5e
   end
 
   def discount_error!
@@ -37,56 +28,12 @@ module FinanceHelper
     html = <<-HTML
      <div class="alert alert-danger alert-dismissable">
       <ul>#{messages}</ul>
-<<<<<<< HEAD
     </div>
-=======
-     </div>
->>>>>>> b96094dbbc906571a04744ef07e3a0a833e26c5e
     HTML
     html.html_safe
   end
 
-<<<<<<< HEAD
   def expense_error!
-=======
-  def collect_category1
-    @categories.collect { |c| [c.name, c.id] }
-  end
-
-  def collect_fee
-    @categories.collect { |c| [c.name, c.id] }
-  end
-
-  def collect_fee_sub_batch
-  	@batches.collect { |b| [b.course.course_name + ' ' + b.course.section_name + '-' + b.name, b.id] }
-  end
-
-  def collect_fee_date
-    @collections.collect { |c| [c.name + ' ' + c.created_at.to_date.to_s, c.id] }
-  end
-
-  def collection_date
-    @collections.uniq.collect { |c| [c.name + ' ' + c.created_at.to_date.to_s, c.id] }
-  end
-
-  def collection_structure
-  	@collections.uniq.collect { |c| [c.name + ' ' + c.created_at.to_date.to_s, c.id] }
-  end
-
-  def collect_fee_course
-    @courses.collect { |c| [c.course_name, c.id] }
-  end
-
-  def collect_batch_choice
-    @batches.collect { |b| [b.course.course_name + ' ' + b.name, b.id] }
-  end
-
-  def collect_collection_date
-    @collections.collect { |c| [c.name + ' ' + c.created_at.to_date.to_s, c.id] }
-  end
-
-  def transaction_errors!
->>>>>>> b96094dbbc906571a04744ef07e3a0a833e26c5e
     return if @transaction.errors.empty?
     messages = @transaction.errors.full_messages.map { |m| content_tag(:li, m) }.join
     html = <<-HTML
@@ -97,11 +44,7 @@ module FinanceHelper
     html.html_safe
   end
 
-<<<<<<< HEAD
   def income_error!
-=======
-  def income_errors!
->>>>>>> b96094dbbc906571a04744ef07e3a0a833e26c5e
     return if @transaction.errors.empty?
     messages = @transaction.errors.full_messages.map { |m| content_tag(:li, m) }.join
     html = <<-HTML
@@ -112,7 +55,6 @@ module FinanceHelper
     html.html_safe
   end
 
-<<<<<<< HEAD
   def collect_catagory
     @categories.collect { |c| [c.name, c.id] }
   end
@@ -136,10 +78,9 @@ module FinanceHelper
   def collect_salary_month
     @salary_months.map {|c| [c.salary_date.strftime("%B %Y"),c.salary_date.strftime("%Y-%m-%d")]}
   end
-end
-=======
+
   def donation_errors!
-     return if @donation.errors.empty?
+    return if @donation.errors.empty?
     messages = @donation.errors.full_messages.map { |m| content_tag(:li, m) }.join
     html = <<-HTML
      <div class="alert alert-danger alert-dismissable">
@@ -149,4 +90,3 @@ end
     html.html_safe
   end
 end
->>>>>>> b96094dbbc906571a04744ef07e3a0a833e26c5e
