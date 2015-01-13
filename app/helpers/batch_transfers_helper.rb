9a03732bf68e37d4612486a952c1666da67c2c7b
Anuja <anujaraut24@gmail.com>
2015-01-13 16:19:39 +0530

    Blank database defect resolved

--- app/helpers/batch_transfers_helper.rb
+++ app/helpers/batch_transfers_helper.rb
@@ -1,12 +1,10 @@
 # BatchTransfer Helper
 module BatchTransfersHelper
-  def collect_course
-    return if @courses.empty?
+  def collect_course_setting
     @courses.all.collect { |p| [p.course_name, p.id] }
   end
 
   def collect_batchtransfer
-    return if @batchs.empty?
     @batchs.collect { |b| [b.full_name, b.id] }
   end
 
--- app/helpers/grading_levels_helper.rb
+++ app/helpers/grading_levels_helper.rb
@@ -1,8 +1,7 @@
 # Grading Level Helper
 module GradingLevelsHelper
   def collect_batch
-    return if @batches.empty?
-    @batches.collect { |b| [b.full_name, b.id] }
+   @batches.collect { |b| [b.full_name, b.id] }
   end
 
   def grading_not_found!
