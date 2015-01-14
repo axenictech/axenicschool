42c64ede5da360e1bd9ca4cec5a175964c02498f
Your Name <you@example.com>
2015-01-14 22:18:36 +0530

    defect resolved of employee module

--- app/models/employee_grade.rb
+++ app/models/employee_grade.rb
@@ -2,9 +2,9 @@ class EmployeeGrade < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 1, maximum: 20 }, format: { with: /\A[a-z A-Z+-]+\z/, message: 'only allows letter' }
 
   validates :priority,  numericality: { only_integer: true, greater_than: 0, less_than: 20 }, uniqueness: true
-  validates :max_hours_day, numericality: { only_integer: true, greater_than: 0, less_than: 20 }
+  validates :max_hours_day, numericality: { only_integer: true, greater_than_equal: 0, less_than: 20 }
 
-  validates :max_hours_week,  numericality: { only_integer: true, greater_than: 0, less_than: 30 }
+  validates :max_hours_week,  numericality: { only_integer: true, greater_than_equal: 0, less_than: 30 }
   scope :is_status, -> { where(status: true).order(:name) }
   scope :not_status, -> { where(status: false).order(:name) }
   scope :shod, ->(id) { where(id: id).take }
