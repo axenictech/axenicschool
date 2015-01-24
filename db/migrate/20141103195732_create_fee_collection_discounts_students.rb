688577d184fe715afac8a10612d638401ec29747
ravikiran kadam <ravikirankadam.1991@gmail.com>
2015-01-24 09:53:29 +0530

    pull for placement exam module

--- db/migrate/20141103195732_create_fee_collection_discounts_students.rb
+++ db/migrate/20141103195732_create_fee_collection_discounts_students.rb
@@ -3,7 +3,6 @@ class CreateFeeCollectionDiscountsStudents < ActiveRecord::Migration
     create_table :fee_collection_discounts_students do |t|
       t.references :student, index: true
       t.references :fee_collection_discount, [index: true, name: 'discount_id']
-
       t.timestamps
     end
   end
