97a4d59720c2983829b3b0a6d3a244fce3ee193b
Anuja <anujaraut24@gmail.com>
2015-01-27 16:44:33 +0530

    ERP

--- app/models/placement_exam.rb
+++ app/models/placement_exam.rb
@@ -1,3 +1,6 @@
 class PlacementExam < ActiveRecord::Base
   has_many :weightages
+  validates :company_id, presence: true
+  validates :question_count, presence: true, numericality: { only_integer: true }
+
 end
